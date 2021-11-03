cbf00fb91529178bbade1babcdee7716fb2d60df
Guillaume Serpaud <guillaume.serpaud@epita.fr>
2021-11-03 21:56:33 +0100

    version 1

--- expense.py
+++ expense.py
@@ -1,5 +1,7 @@
+import csv
+import re
 from PyInquirer import prompt
-
+"""
 expense_questions = [
     {
         "type":"input",
@@ -18,11 +20,78 @@ expense_questions = [
     },
 
 ]
+"""
+
+spender = ""
+
+
+def retrieve_users():
+    res = []
+    with open('users.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in spamreader:
+            res.append(row[0])
+    return res
+
+def retrieve_involved():
+    res = []
+    with open('users.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in spamreader:
+            if row[0] != spender:
+                res.append({"name":row[0]})
+    return res
+
+def isNumber(amount):
+    try:
+        float(amount)
+    except:
+        return False
+
+
+expense_questions = [
+    {
+        "type":"input",
+        "name":"amount",
+        "message":"New Expense - Amount: ",
+        'validate': lambda amount: 'Amount must be a number' \
+            if isNumber(amount) == False else True
+    },
+    {
+        "type":"input",
+        "name":"label",
+        "message":"New Expense - Label: ",
+    },
+    {
+        "type":"list",
+        "name":"spender",
+        "message":"New Expense - Spender: ",
+        "choices": retrieve_users()
+    },
+    {
+        "type":"checkbox",
+        "message":"Select people who are involved in the expense: ",
+        "name":"involved-users",
+        "choices": retrieve_involved()
+    }
+]
 
 
 
 def new_expense(*args):
+    retrieve_users()
     infos = prompt(expense_questions)
+    isName = False
+    with open('expense_report.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+            if row == ['amount,label,spender,involved-people']:
+                isName = True
+    with open('expense_report.csv', 'a') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        if not isName:
+            spamwriter.writerow(["amount", "label", "spender","involved-people"])
+        spamwriter.writerow([infos["amount"], infos["label"], infos["spender"], infos["involved-users"]])
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
     print("Expense Added !")
     return True
--- user.py
+++ user.py
@@ -1,8 +1,19 @@
 from PyInquirer import prompt
+import csv
 user_questions = [
+    {
+        "type":"input",
+        "name":"name",
+        "message":"New User - Name"
+    }
 
 ]
 
-def add_user():
+def add_user(*args):
+    infos = prompt(user_questions)
+    with open('users.csv', 'a', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow([infos["name"]])
+    print("User Added !")
     # This function should create a new user, asking for its name
-    return
+    return True
